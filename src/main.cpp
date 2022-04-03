3fa397cc86bbff27f9be9f2e790ac08d0da38c3f
kartoffeln2112 <rachel.pitts42@gmail.com>
2022-04-03 16:19:50 -0500

    adding pause button

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@ wific wifi;
 
 const char* TAG = "MAIN";
 const char* ROOM_NAME = "room1";
-char draw_string[1024];
+const int SEND_INTERVAL = 10000;
+bool pauseConnect = false;
+long unsigned int currentMillis;
 
 bool env_sensor(float*, float*, float*);
 float earth_sensor();
@@ -37,45 +39,70 @@ void setup() {
   // setup EARTH sensor
   pinMode(26, INPUT);
   dacWrite(25,0);
+
+  currentMillis = millis();
 }
 
 void loop() {
+  M5.update();
   float temp, humidity, pressure, rh;
   StaticJsonDocument<JSON_OBJECT_SIZE(6)> jdata;
   String jout;
-  
-  // attempt to retrieve ENV III sensor info
-  bool sensor_success = env_sensor(&temp, &humidity, &pressure);
 
-  // retrieve earth_sensor info
-  rh = earth_sensor();
-
-  // skip loop if sensor information wasn't retrieved
-  if (sensor_success)
+  if (M5.BtnC.wasPressed())
   {
-    // print status to screen
-    M5.Lcd.clear();
-    M5.Lcd.setCursor(0,0);
-    long unsigned int timestamp = wifi.getTimestamp();
-    M5.Lcd.printf("Temperature: %2.2f*C  \nHumidity: %0.2f%%  \nPressure: %0.2fPa\r\n", temp, humidity, pressure);
-    M5.Lcd.printf("Analog Read: %f\n", rh);
-
-    // serialize data into JSON
-    jdata["Time"].set(timestamp);
-    jdata["Room"].set(ROOM_NAME);
-    jdata["Temperature"].set(temp);
-    jdata["Humidity"].set(humidity);
-    jdata["Pressure"].set(pressure);
-    jdata["Moisture"].set(rh);
-
-    serializeJson(jdata, jout); 
-
-    wifi.checkMQTTConnect();
-    wifi.publish(jout.c_str());
-
-    delay(10000);
+    pauseConnect = !pauseConnect;
+    if (pauseConnect)
+    {
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0,0);
+      M5.Lcd.printf("Sensor reading and connection Paused.\n");
+    }
+    else
+    {
+      M5.Lcd.printf("Resuming connection...");
+    }
   }
 
+  if (((millis() - currentMillis) >= SEND_INTERVAL) && !pauseConnect)
+  {
+    currentMillis = millis();
+    // attempt to retrieve ENV III sensor info
+    bool sensor_success = env_sensor(&temp, &humidity, &pressure);
+
+    // retrieve earth_sensor info
+    rh = earth_sensor();
+
+    // skip loop if sensor information wasn't retrieved
+    if (sensor_success)
+    {
+      // print status to screen
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0,0);
+      long unsigned int timestamp = wifi.getTimestamp();
+      M5.Lcd.printf("Temperature: %2.2f*C  \nHumidity: %0.2f%%  \nPressure: %0.2fPa\r\n", temp, humidity, pressure);
+      M5.Lcd.printf("Analog Read: %f\n", rh);
+
+      // serialize data into JSON
+      jdata["Time"].set(timestamp);
+      jdata["Room"].set(ROOM_NAME);
+      jdata["Temperature"].set(temp);
+      jdata["Humidity"].set(humidity);
+      jdata["Pressure"].set(pressure);
+      jdata["Moisture"].set(rh);
+
+      serializeJson(jdata, jout); 
+
+      wifi.checkMQTTConnect();
+      wifi.publish(jout.c_str());
+    }
+  }
+  
+  else if (((millis() - currentMillis) >= SEND_INTERVAL))
+  {
+    currentMillis = millis();
+    wifi.checkMQTTConnect();
+  }
 }
 
 
@@ -99,7 +126,7 @@ float earth_sensor()
 {
   float percent;
   uint16_t anaRead = analogRead(36);
-  percent = abs((int)anaRead - 4095) / 100.0; 
+  percent = map(anaRead, 4095, 0, 0, 100); 
 
   return percent;
 }
